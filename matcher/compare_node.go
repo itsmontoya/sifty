c530b9b4779051c4bc6c8d9a32bdfb035dec8dde
Josh Montoya <josh@montoya.io>
2026-04-01 15:18:38 -0700

    implement compareNode

---
+++ matcher/compare_node.go
@@ -0,0 +1,44 @@
+package matcher
+
+import "github.com/itsmontoya/sifty/query"
+
+func makeRangeNode(in *query.RangeExpr) (out compareNode, err error) {
+	out.field = in.Field
+	out.gt = in.Gt
+	out.gte = in.Gte
+	out.lt = in.Lt
+	out.lte = in.Lte
+	return out, nil
+}
+
+type compareNode struct {
+	field string
+
+	eq  any
+	gt  any
+	gte any
+	lt  any
+	lte any
+}
+
+func (n compareNode) eval(doc DocView) (ok bool, err error) {
+	var val any
+	if val, ok, err = doc.Get(n.field); !ok || err != nil {
+		return ok, err
+	}
+
+	switch {
+	case n.eq != nil:
+		return isEqualTo(val, n.eq), nil
+	case n.gt != nil:
+		return isGreaterThan(val, n.gt), nil
+	case n.gte != nil:
+		return isGreaterThanOrEqualTo(val, n.gt), nil
+	case n.lt != nil:
+		return isLessThan(val, n.gt), nil
+	case n.lte != nil:
+		return isLessThanOrEqualTo(val, n.gt), nil
+	default:
+		return false, nil
+	}
+}
