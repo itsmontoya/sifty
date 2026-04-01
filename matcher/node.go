82b441ed97dca076b95a3115d0bb2ece5443c8f3
Josh Montoya <josh@montoya.io>
2026-04-01 15:14:54 -0700

    implement node

---
+++ matcher/node.go
@@ -0,0 +1,32 @@
+package matcher
+
+import (
+	"errors"
+
+	"github.com/itsmontoya/sifty/query"
+)
+
+func toNode(in query.Clause) (n node, err error) {
+	switch {
+	case in.IsZero():
+		return makeAnyNode()
+	case len(in.And) > 0:
+		return makeAndNode(in.And)
+	case len(in.Or) > 0:
+		return makeAndNode(in.Or)
+	case in.Not != nil:
+		return makeNotNode(in.Not)
+	case in.Term != nil:
+		return makeTermNode(in.Term)
+	case in.Contains != nil:
+		return makeContainsNode(in.Contains)
+	case in.Range != nil:
+		return makeRangeNode(in.Range)
+	default:
+		return nil, errors.New("invalid clause, needs to have at least one set")
+	}
+}
+
+type node interface {
+	eval(doc DocView) (bool, error)
+}
