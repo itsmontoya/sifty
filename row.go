39c6051946d3f4cc8c7ea78a524b708cc78bb729
Josh Montoya <josh@montoya.io>
2026-04-05 18:48:49 -0700

    implement Row.append and makeRow

--- row.go
+++ row.go
@@ -1,8 +1,22 @@
 package sifty
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+func makeRow(v any) (r Row) {
+	r.Timestamp = time.Now()
+	r.Value = v
+	return r
+}
 
 type Row struct {
 	Timestamp time.Time `json:"timestamp"`
 	Value     any       `json:"value"`
+}
+
+func (r *Row) append(w io.Writer) error {
+	return json.NewEncoder(w).Encode(r)
 }
